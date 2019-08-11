92cd0354fed2aa60cd104a05f38fac3dc968a000
felipesmith <fsmith95@hotmail.com>
2019-08-11 12:31:55 -0300

    Permisos Cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(urlDB)
 .catch(error => console.log(error));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", '*,http://localhost:4200');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
