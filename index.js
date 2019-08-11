375756aae9ec6b26c61f367c7f71aac68a25df7b
felipesmith <fsmith95@hotmail.com>
2019-08-11 12:06:24 -0300

    Permisos Cors

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ mongoose.connect(urlDB)
 .then(() => console.log("GreenMind online."))
 .catch(error => console.log(error));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
