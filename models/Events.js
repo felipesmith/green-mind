1a1aebab8a8fd114d4b1a45e7715468bb83e6d80
felipesmith <fsmith95@hotmail.com>
2019-08-31 00:50:10 -0300

    Default values

--- models/Events.js
+++ models/Events.js
@@ -6,8 +6,8 @@ let eventsSchema = new Schema({
     title:      { type: String, required: true },
     description:   { type: String, required: true },
     location:    { type: String, required: true },
-    date:     { type: Date, required: true },
-    score:      { type: Number, required: true },
+    date:     { type: Date, default: Date.now, required: true },
+    score:      { type: Number, default: 0, required: false },
     type:     { type: String, required: true }
 });
 
