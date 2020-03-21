7c4237cb70ded5ad95eb3507c938f15125dfb2b8
felipesmith <fsmith95@hotmail.com>
2020-03-21 15:54:28 -0300

    'img'

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let express         = require('express')
 let bodyParser      = require('body-parser');
 let cors            = require('cors');
 let mongoose        = require('mongoose');
-
+let multer = require('multer');
 let usersRoutes     = require("./routes/usersRoutes")
 let commentsRoutes  = require("./routes/commentsRoutes");
 let eventsRoutes  = require("./routes/eventsRoutes");
 let postsRoutes  = require("./routes/postsRoutes");
+let upload = multer({dest: __dirname + '/uploads/images'});
 
 let app             = express();
 
@@ -19,12 +20,49 @@ mongoose.set('useCreateIndex',      true);
 mongoose.connect(urlDB)
 .then(() => console.log("GreenMind online."))
 .catch(error => console.log(error));
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.static('public'));
+app.post('/upload', upload.single('photo'), (req, res) => {
+    if(req.file) {
+        res.json(req.file);
+    }
+    else throw 'error';
+});
+
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
