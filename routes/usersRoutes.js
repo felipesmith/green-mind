6203bd0645fc053306e9371a3cc1b03b555e1424
felipesmith <fsmith95@hotmail.com>
2020-03-23 16:13:54 -0300

    Prueba

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,8 @@
 let router = require('express').Router();
 let multer = require('multer');
-const upload = multer({
-  dest: "../temp"
-});
+// const upload = multer({
+//   dest: "../temp"
+// });
 let path = require('path');
 let {allUsers,
     createUser,
@@ -75,34 +75,34 @@ router.post('/update', async function(req, res) {
     }
 });
 
-router.post(
-  "/images/upload",
-  upload.single("profile_image"),
-  (req, res) => {
-    const tempPath = req.file.path;
-    const targetPath = path.join(__dirname, "./uploads/image.png");
-
-    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-      fs.rename(tempPath, targetPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(200)
-          .contentType("text/plain")
-          .end("File uploaded!");
-      });
-    } else {
-      fs.unlink(tempPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(403)
-          .contentType("text/plain")
-          .end("Only .png files are allowed!");
-      });
-    }
-  }
-);
+// router.post(
+//   "/images/upload",
+//   upload.single("profile_image"),
+//   (req, res) => {
+//     const tempPath = req.file.path;
+//     const targetPath = path.join(__dirname, "./uploads/image.png");
+//
+//     if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+//       fs.rename(tempPath, targetPath, err => {
+//         if (err) return handleError(err, res);
+//
+//         res
+//           .status(200)
+//           .contentType("text/plain")
+//           .end("File uploaded!");
+//       });
+//     } else {
+//       fs.unlink(tempPath, err => {
+//         if (err) return handleError(err, res);
+//
+//         res
+//           .status(403)
+//           .contentType("text/plain")
+//           .end("Only .png files are allowed!");
+//       });
+//     }
+//   }
+// );
 
 
 
