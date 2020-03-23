d2412c8d722c5e31961aa909046c0a27ec506620
felipesmith <fsmith95@hotmail.com>
2020-03-23 14:56:16 -0300

    Prueba Imagen

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,6 @@
 let router = require('express').Router();
 let multer = require('multer');
 let path = require('path');
-let Resize = require('../Resize');
 let {allUsers,
     createUser,
     logInUser,
@@ -73,14 +72,14 @@ router.post('/update', async function(req, res) {
 });
 
 
-router.post('/post', upload.single('image'), async function (req, res) {
-  const imagePath = path.join(__dirname, '/public/images');
-  const fileUpload = new Resize(imagePath);
-  if (!req.file) {
-    res.status(401).json({error: 'Please provide an image'});
-  }
-  const filename = await fileUpload.save(req.file.buffer);
-  return res.status(200).json({ name: filename });
-});
+// router.post('/post', upload.single('image'), async function (req, res) {
+//   const imagePath = path.join(__dirname, '/public/images');
+//   const fileUpload = new Resize(imagePath);
+//   if (!req.file) {
+//     res.status(401).json({error: 'Please provide an image'});
+//   }
+//   const filename = await fileUpload.save(req.file.buffer);
+//   return res.status(200).json({ name: filename });
+// });
 
 module.exports = router;
