5cd055f10a1923a37e09ce5bbd00fe68451ae9c6
felipesmith <fsmith95@hotmail.com>
2020-04-04 16:33:58 -0300

    Prueba imagen

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -96,7 +96,7 @@ router.post(
   "/images/upload",
   upload.single("profile_image"),
   (req, res) => {
-    console.log(req);
+    console.log(req.body);
     const tempPath = req.file.path;
     // const targetPath = path.join(__dirname, "../uploads/image.png");
       const targetPath = `image_${new Date().getTime()}.png`;
