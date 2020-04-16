2d77cf26480dfaad6fbf084c5b6b83d17031a87d
felipesmith <fsmith95@hotmail.com>
2020-04-16 19:11:27 -0300

    Fix

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -36,13 +36,13 @@ searchPost = async (_id ) => {
 
 updatePhoto = async(_id,image)=>{
   console.log(_id);
-  let post = await Posts.find({_id}).sort({ '_id': -1 });
+  let post = await Posts.find({_id});
   console.log("Esta es la imagen"+image);
   console.log("Este es el post" +post);
-  post[0].image = image;
+  post.image = image;
   console.log("Este es el post actualiza3" +post);
-  await post[0].save();
-  return {post[0]};
+  await post.save();
+  return {post};
 };
 
 rankPosts = (posts) => {
