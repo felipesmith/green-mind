c3a2bb1f9ba3713063cc051045362ba4e753ff32
felipesmith <fsmith95@hotmail.com>
2020-04-16 19:13:36 -0300

    Fix

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -36,7 +36,7 @@ searchPost = async (_id ) => {
 
 updatePhoto = async(_id,image)=>{
   console.log(_id);
-  let post = await Posts.find({_id});
+  let post = await Posts.findOne({_id});
   console.log("Esta es la imagen"+image);
   console.log("Este es el post" +post);
   post.image = image;
